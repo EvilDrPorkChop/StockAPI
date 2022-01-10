2b10b4a7bd971161c073f81557cc512c4266be88
Ben Staite <ben.staite1999@hotmail.co.uk>
2022-01-10 19:56:19 +0000

    More bits and bobs

---
+++ TradingAPI/StockEnv.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 00:36:36 2021
+
+@author: Benst
+"""
+import pandas
+import yfinance as yf
+from pandas_datareader import data as pdr
+import copy
+
+
+class State:
+    def serialize(self):
+        return {
+            'Date': self.Date,
+            'Price': self.Price,
+            'Volume': self.Volume,
+            'Shares': self.Shares,
+            'Balance': self.Balance,
+            'Action': self.Action,
+            'Value': self.Value,
+        }
+
+    def __init__(self, date, price, volume, shares, balance):
+        self.Date = date
+        self.Price = float(price)
+        self.Volume = int(volume)
+        self.Shares = int(shares)
+        self.Balance = int(balance)
+        self.Action = ""
+        self.Value = float(self.Shares * self.Price + self.Balance)
+
+    def calcValue(self):
+        self.Value = float(self.Shares * self.Price + self.Balance)
+
+
+class StockEnv:
+
+    def __init__(self, ticker, startBal, period, interval):
+        self.ticker = ticker
+        self.startbal = int(startBal)
+        self.period = period
+        self.interval = interval
+        self.Finished = False
+        self.TimeStep = 0
+        data = self.LoadData()
+        self.Data = data
+        firstrow = self.Data.iloc[0]
+        self.State = State(firstrow['Date'], firstrow['Open'], firstrow['Volume'], 0, startBal)
+        self.States = []
+
+    def LoadData(self):
+        yf.pdr_override()
+        ftse = yf.Ticker(self.ticker)
+        data = ftse.history(period=self.period, interval=self.interval).dropna()
+        data = data.reset_index()
+        return data
+
+    def Step(self, action):
+        if action == 'Buy':
+            self.Buy()
+        elif action == 'Sell':
+            self.Sell()
+        else:
+            self.Hold()
+
+        self.AdvanceTime()
+
+    def AdvanceTime(self):
+        self.State.calcValue()
+        self.States.append(copy.deepcopy(self.State))
+
+        self.TimeStep += 1
+
+        if self.TimeStep >= len(self.Data.index) - 1:
+            self.Finished = True
+        else:
+            row = self.Data.iloc[self.TimeStep]
+            self.State = State(date=(row['Date']), price=(row['Open']), volume=(row['Volume']),
+                               shares=self.State.Shares, balance=self.State.Balance)
+
+    def Buy(self):
+        if self.State.Balance - self.State.Price < 0:
+            self.Hold()
+        else:
+            self.State.Balance -= self.State.Price
+            self.State.Shares += 1
+            self.State.Action = "Buy"
+
+    def Hold(self):
+        self.State.Action = "Hold"
+
+    def Sell(self):
+        if self.State.Shares < 1:
+            self.Hold()
+        else:
+            self.State.Balance += self.State.Price
+            self.State.Shares -= 1
+            self.State.Action = "Sell"
---
+++ TradingAPI/Trader.py
@@ -0,0 +1,72 @@
+import StockEnv
+
+
+def calcValue(state):
+    return float(state.Shares * state.Price + state.Balance)
+
+def getAverage(prices):
+    total = 0
+    for p in prices:
+        total += p
+    return total/len(prices)
+
+def calcMACD(prices):
+    n1 = 5
+    n2 = 10
+    n3 = 8
+    ema1 = []
+    ema2 = []
+    ema3 = []
+
+    ema = getAverage(prices[0:n1])
+
+    for i in range(0,len(prices)):
+        if i >= n1:
+            ema = prices[i] * (2 / n1 + 1) + (ema * (1 - (2 / (n1 + 1))))
+        ema1.append(ema)
+
+    ema = getAverage(prices[0:n2])
+
+    for i in range(0, len(prices)):
+        if i >= n2:
+            ema = prices[i] * (2 / n2 + 1) + (ema * (1 - (2 / (n2 + 1))))
+        ema2.append(ema)
+
+    macDs = []
+    for i in range(0, len(prices)):
+        macDs.append(ema2[i]-ema1[i])
+
+    ema = getAverage(macDs[0:n3])
+
+    for i in range(0, len(prices)):
+        if i >= n3:
+            ema = prices[i] * (2 / n3 + 1) + (ema * (1 - (2 / (n3 + 1))))
+        ema3.append(ema)
+
+
+
+
+    return macDs, ema3
+
+class Trader:
+
+    def __init__(self, ticker, startbal, period, interval):
+        self.lastboughtAt = float(0)
+        self.env = StockEnv.StockEnv(ticker, startbal, period, interval)
+        self.prices = []
+        self.MACD = 0
+
+    def decide(self, state):
+        self.prices.append(state.Price)
+
+        if state.Balance > state.Price and state.Price < getAverage(self.prices):
+            self.lastboughtAt = state.Price
+            return 'Buy'
+        elif state.Shares > 0 and state.Price>self.lastboughtAt:
+            return 'Sell'
+        else:
+            return 'Hold'
+
+    def Run(self):
+        while not self.env.Finished:
+            self.env.Step(self.decide(self.env.State))
